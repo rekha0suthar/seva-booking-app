f8b170349d251cd3054d9c9192e0c37c427a6e7b
rekha0suthar <rekha0suthar@gmail.com>
2025-05-07 09:57:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
